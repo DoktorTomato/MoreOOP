20a7fba2cd488663eb5660d356c78008e63e5897
DoktorTomato <96630602+DoktorTomato@users.noreply.github.com>
2024-09-30 20:49:25 +0300

    Added test to Demo file and fixed GameManager

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,6 +1,15 @@
-// import lotr.Hobbit;
+import lotr.GameManager;
+import lotr.CharacterFactory;
+import lotr.Character;
 
-// public class Demo {
-//     Hobbit frodo = new Hobbit();
-//     frodo.kick(frodo);
-// }
+public class Demo {
+    public static void main(String[] args) {
+        CharacterFactory cf = new CharacterFactory();
+        GameManager gm = new GameManager();
+        Character c1 = cf.createCharacter();
+        System.out.println(c1.toString() + "was created");
+        Character c2 = cf.createCharacter();
+        System.out.println(c1.toString() + "was created");
+        gm.fight(c1, c2);   
+    }
+}
--- src/main/java/lotr/GameManager.java
+++ src/main/java/lotr/GameManager.java
@@ -2,11 +2,28 @@ package lotr;
 
 public class GameManager {
     public void fight(Character c1, Character c2){
+        int counter = 0;
         while (c1.getHp() > 0 && c2.getHp() > 0){
-            c1.kick(c2);
             System.out.println(c1.getClass().getSimpleName()+" kicks "+c2.getClass().getSimpleName());
+            c1.kick(c2);
             System.out.println(c1.getClass().getSimpleName()+ "'s HP is " +c1.getHp());
             System.out.println(c2.getClass().getSimpleName()+ "'s HP is " +c2.getHp());
+
+            System.out.println("======================================================");
+
+            System.out.println(c2.getClass().getSimpleName()+" kicks "+c1.getClass().getSimpleName());
+            c2.kick(c1);
+            System.out.println(c1.getClass().getSimpleName()+ "'s HP is " +c1.getHp());
+            System.out.println(c2.getClass().getSimpleName()+ "'s HP is " +c2.getHp());
+
+            counter += 1;
+
+            if (counter >= 10 && c1.getClass().isInstance(c2)){
+                System.out.println("======================================================");
+                System.out.println("Two hobbits were crying forever and forever untill the end of time");
+                break;
+            }
         }
+        System.out.println("=======================THE END========================");
     }
 }
